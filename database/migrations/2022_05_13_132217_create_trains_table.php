891784d8bcff110f33960353b001d4201372a33b
cochy98 <cosmoferrigno@gmail.com>
2022-05-13 16:16:27 +0200

    Add migrations

---
+++ database/migrations/2022_05_13_132217_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('agency', 50);
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->time('departure_time', 0);
+            $table->time('arrival_time', 0);
+            $table->string('train_code', 5);
+            $table->tinyInteger('number_of_carriages');
+            $table->boolean('in_time')->nullable();
+            $table->boolean('is_deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
