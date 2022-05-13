e08fc39c92b584af080f03b234b547264269ce2e
cochy98 <cosmoferrigno@gmail.com>
2022-05-13 15:08:22 +0200

    first commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  {{-- Visualizzo con la funzione '@yield' il titolo della pagina corrente --}}
+  <title>Laravel Migration Seeder - @yield('title')</title>
+
+  {{-- Importo il foglio di stile --}}
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+  <main>
+    {{-- Visualizzo la pagina corrente --}}
+    @yield('main')
+  </main>
+  
+</body>
+</html>
---
+++ resources/views/trains/index.blade.php
@@ -0,0 +1,10 @@
+{{-- Implemento la pagina main --}}
+@extends('layouts.main')
+
+{{-- Do un titolo a questa section --}}
+@section('title', 'Trains')
+
+{{-- Punto dove implemento questa section nella pagina principale --}}
+@section('main')    
+  <h1>prova</h1>
+@endsection
