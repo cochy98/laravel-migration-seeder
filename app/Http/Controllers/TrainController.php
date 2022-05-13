ee9b9e6fa89b188abdebebd5b7c07e2044937e37
cochy98 <cosmoferrigno@gmail.com>
2022-05-13 17:58:36 +0200

    Add 'Train' Model

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -4,10 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+// § Importo la classe 'Train'
+use App\Train;
+
 class TrainController extends Controller
 {
     public function index()
     {
-        return view('trains.index');
+        // § Recupero tutte le instanze di 'Train' e le passo alla view interessata
+        $trains = Train::all();
+        $data = [
+            'trains' => $trains
+        ];
+        return view('trains.index', $data);
     }
 }
