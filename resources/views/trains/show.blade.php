695c94b9e076a70962face7d9eab5b0fbfe93217
cochy98 <cosmoferrigno@gmail.com>
2022-05-16 21:00:57 +0200

    Modify custom style

--- resources/views/trains/show.blade.php
+++ resources/views/trains/show.blade.php
@@ -4,17 +4,24 @@
 
 @section('main')
 <div class="container">
-  <h1 class="py-4">{{ $message }}</h1>
-  <p scope="col">Codice Treno: <span>{{ $train->train_code }}</span></p>
-  <p scope="col">Operato da: <span>{{ $train->agency }}</span></p>
-  <p scope="col">Stazione partenza: <span>{{ $train->departure_station }}</span></p>
-  <p scope="col">Stazione arrivo: <span>{{ $train->arrival_station }}</span></p>
-  <p scope="col">Data: <span>{{ $train->departure_date }}</span></p>
-  <p scope="col">Orario partenza: <span>{{ $train->departure_time }}</span></p>
-  <p scope="col">Orario Arrivo: <span>{{ $train->arrival_time }}</span></p>
-  <p scope="col">Numero carrozze: <span>{{ $train->number_of_carriages }}</span></p>
-  <p scope="col">In Orario: <span>{{ $train->in_time }}</span></p>
-  <p scope="col">Cancellato: <span>{{ $train->is_deleted }}</span></p>
+  <div class="row">
+    <div class="col-12">
+      <h1 class="pt-5">{{ $message }}</h1>
+    </div>
+  </div>
+
+  <div class="details col-12">
+    <p><strong>Codice Treno: </strong><span>{{ $train->train_code }}</span></p>
+    <p><strong>Operato da: </strong><span>{{ $train->agency }}</span></p>
+    <p><strong>Stazione partenza: </strong><span>{{ $train->departure_station }}</span></p>
+    <p><strong>Stazione arrivo: </strong><span>{{ $train->arrival_station }}</span></p>
+    <p><strong>Data: </strong><span>{{ $train->departure_date }}</span></p>
+    <p><strong>Orario partenza: </strong><span>{{ $train->departure_time }}</span></p>
+    <p><strong>Orario Arrivo: </strong><span>{{ $train->arrival_time }}</span></p>
+    <p><strong>Numero carrozze: </strong><span>{{ $train->number_of_carriages }}</span></p>
+    <p><strong>In Orario: </strong><span>{{ $train->in_time }}</span></p>
+    <p><strong>Cancellato: </strong><span>{{ $train->is_deleted }}</span></p>
+  </div>
 </div>
     
 @endsection
