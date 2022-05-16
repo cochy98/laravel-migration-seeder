68f34d0b93d4a8ffbe662e940f6b3a9b923e4366
cochy98 <cosmoferrigno@gmail.com>
2022-05-16 17:28:13 +0200

    Fix trains seeder

--- database/seeds/TrainsTableSeeder.php
+++ database/seeds/TrainsTableSeeder.php
@@ -14,27 +14,23 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for ($i = 0; $i < 10; $i++) {
+        for ($i = 0; $i < 100; $i++) {
             $train = new Train();
             $train->agency = $faker->word();
             $train->departure_station = $faker->sentence(3);
             $train->arrival_station = $faker->sentence(3);
             $train->departure_time = $faker->time();
 
-            $arrivalTime = $faker->time();
-            if (!$train->departure_time > $arrivalTime) {
-                $train->arrival_time = $arrivalTime;
-            }
+            $arrivalTime = explode(':', $train->departure_time);
+            $arrivalTime[0]++;
+            $train->arrival_time = join(':', $arrivalTime);
 
             $train->train_code = $faker->randomNumber(5, true);
             $train->number_of_carriages = $faker->numberBetween(2, 13);
             $train->in_time = $faker->numberBetween(0, 1);
             $train->is_deleted = $faker->numberBetween(0, 1);
-
-            $departureDate = $faker->date();
-            if ($departureDate >= date('Y-m-d')) {
-                $train->departure_date = $departureDate;
-            }
+            $train->departure_date = $faker->dateTimeBetween('+0 day', '+1 week');
+            $train->save();
         }
     }
 }
